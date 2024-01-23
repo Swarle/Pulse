b26a0efc66c3e458359d7d25888f2da02cf18b52
Swarle <bogdanvalman@gmail.com>
2024-01-23 20:08:23 +0200

    Added validation and mask for input .tel

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,39 @@ const slider = tns({
         });
     });
 
+    function validateForms(form){
+        $(form).validate({
+            errorPlacement: function(error,element){
+                error.insertBefore(element);
+            },
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                tel: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                tel: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        })
+    };
+
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    validateForms('#order form');
+
+    $('input[name=tel]').mask("+7 (999) 999-99-99");
   });
